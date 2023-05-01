df0347edc0fba26f390d2cabf7715741fbfbb51b
Oleg Matus <matys_bboy96@ukr.net>
2023-05-02 00:55:05 +0300

    lesson1

---
+++ Lesson1/Task1.1.js
@@ -0,0 +1,43 @@
+// let a = 'hello'
+// let b = 'owu'
+// let c = 'com'
+// let d = 'ua'
+// let e = '1'
+// let f = '10'
+// let g = '-999'
+// let h = '123'
+// let i = '3.14'
+// let j = '2.7'
+// let k = '16'
+// let l = 'true'
+// let m = 'false'
+
+// --------------------------------hw1.1-------------------
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+// console.log(j)
+// console.log(k)
+// console.log(l)
+// console.log(m)
+// ------------------------------hw1.2---------------
+
+// let firstName = 'Oleg'
+// let middleName = 'Vasyliovych'
+// let lastName = 'Matus'
+//
+// let person = `${firstName} ${middleName} ${lastName}`
+// console.log(person)
+// ----------------------hw1.3----------------------
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
